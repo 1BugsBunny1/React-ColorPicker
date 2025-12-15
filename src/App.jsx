431ae97d219777afd832bde8cccc5709cde64a8f
= <=>
2025-12-15 22:08:47 +0300

    add tictaktoe comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Condition from "./components/Condition.jsx";
 import ColorPicker from "./ColorPicker.jsx";
+import TicTacToe from "./components/TicTakToe.jsx";
 
 function App() {
   return (
